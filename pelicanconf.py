1a994ae960a513793f9cdf9a52caf6afdea80784
Meven <mgapb2@cam.ac.uk>
2024-09-20 12:03:41 +0100

    nit

--- pelicanconf.py
+++ pelicanconf.py
@@ -78,7 +78,7 @@ author_webpage = {
              "Kenji Maillard": "https://kenji.maillard.blue/",
              "Matthieu Sozeau": "https://sozeau.gitlabpages.inria.fr/www/",
              "Yannick Forster": "https://yforster.github.io/",
-             "The MetaCoq Team": "https://metacoq.github.io/",
+             "The MetaCoq Team": "https://metacoq.github.io/#team--credits",
              "Pierre-Marie Pédrot": "https://www.pédrot.fr/",
              "Arthur Adjedj": "https://github.com/arthur-adjedj",
              "Loïc Pujet": "pujet.fr",
